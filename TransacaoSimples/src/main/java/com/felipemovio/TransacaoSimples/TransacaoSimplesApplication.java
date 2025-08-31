302945e6274236f69d8761e89f1b6ce6e8aaf2af
FelipeMovio <felipelsmovio@gmail.com>
2025-08-31 17:27:09 -0300

    iniciando

---
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/TransacaoSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.felipemovio.TransacaoSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoSimplesApplication.class, args);
+	}
+
+}
