06de68b1664f2b33e94b8723d526e12e44396ea6
FelipeMovio <felipelsmovio@gmail.com>
2025-09-02 14:07:01 -0300

    transacaoController e configuracao properties

---
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/TransacaoController.java
@@ -0,0 +1,24 @@
+package com.felipemovio.TransacaoSimples.controller;
+
+import com.felipemovio.TransacaoSimples.services.TransferenciasService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/transfer")
+public class TransacaoController {
+
+    private final TransferenciasService transferenciasService;
+
+    @PostMapping
+    public ResponseEntity<Void> realizarTransacao(@RequestBody TransacaoDTO transacaoDTO){
+        transferenciasService.transferirValores(transacaoDTO);
+        return ResponseEntity.accepted().build();
+    }
+
+}
